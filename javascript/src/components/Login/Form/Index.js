830c7975e916448213895f7f098fd68eba01b545
Filipe Reinert Silva <filipereinertss@gmail.com>
2019-10-02 11:14:18 -0300

    Criado tela de login

--- javascript/src/components/Login/Form/Index.js
+++ javascript/src/components/Login/Form/Index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
+import styles from './login.module.css'; // Import css modules stylesheet as styles
 import * as Yup from "yup";
 
 export const LoginForm = ({onSubmit}) => {
@@ -20,44 +21,58 @@ export const LoginForm = ({onSubmit}) => {
       })}
       onSubmit={onSubmit}
       render={({ errors, status, touched, isSubmitting }) => (
-        <Form className='container'>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <Field
-              name="email"
-              type="text"
-              className={
-                "form-control" +
-                (errors.email && touched.email ? " is-invalid" : "")
-              }
-            />
-            <ErrorMessage
-              name="email"
-              component="div"
-              className="invalid-feedback"
-            />
+        <Form className='container' style={{marginTop: "200px"}}>
+
+          <div className={`${styles.wrapper} ${styles.fadeInDown}`}>
+            <div id="formContent">
+              <div className={`text-center mb-3 ${styles.fadeIn} ${styles.first}`}>
+                <i className="far fa-user-circle fa-5x"></i>
+              </div>
+
+              <div className="form-group">
+                <Field
+                  name="email"
+                  type="text"
+                  placeholder="E-mail"
+                  className={`form-control ${styles.fadeIn} ${styles.second} ${(errors.email && touched.email ? " is-invalid" : "")}`}
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
+
+              <div className="form-group">
+                <Field
+                  name="password"
+                  type="text"
+                  placeholder="Senha"
+                  className={`form-control ${styles.fadeIn} ${styles.third} ${(errors.password && touched.password ? " is-invalid" : "")}`}
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
+
+              <div className="form-group text-center">
+                <Field
+                  type="submit"
+                  placeholder="Senha"
+                  value="Login"
+                  className={`${styles.fadeIn} ${styles.fourth}`}
+                />
+              </div>
+              
+              <div id="formFooter">
+                <a className={`${styles.underlineHover}`} href="#">Esqueceu sua senha?</a>
+              </div>
+
+            </div>
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <Field
-              name="password"
-              type="password"
-              className={
-                "form-control" +
-                (errors.password && touched.password ? " is-invalid" : "")
-              }
-            />
-            <ErrorMessage
-              name="password"
-              component="div"
-              className="invalid-feedback"
-            />
-          </div>
-          <div className="form-group">
-            <button type='submit' disabled={isSubmitting} className="btn btn-primary mr-2">
-              Login
-            </button>
-          </div>
+
         </Form>
       )}
     />
