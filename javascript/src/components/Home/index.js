8edeef055cabc38f0138bc61f87e26539d256411
Matheus Sabino <matheusouzatj@gmail.com>
2019-10-02 09:40:19 -0300

    add css modules

--- javascript/src/components/Home/index.js
+++ javascript/src/components/Home/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './home.module.css'; // Import css modules stylesheet as styles
 import {Link} from 'react-router-dom'
 
 export const Home = ()=>{
   return (
-    <div>
+    <div className={styles.home}>
       <h1>Home</h1>
       <Link to='/login'> Login</Link>
       <Link to='/admin'> admin</Link>
