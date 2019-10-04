18c70e01eba3cdd79dcf2304c20b06e7464cf008
Matheus <matheusouzatj@gmail.com>
2019-10-03 21:41:13 -0300

    remove unused code

--- javascript/src/App.js
+++ javascript/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import {Routes} from './routes'
 
 class App extends React.Component {
--- javascript/src/features/Admin/Product/features/New/index.js
+++ javascript/src/features/Admin/Product/features/New/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { ProductForm } from '../../components/ProductForm/index'
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import request from '../../../../../services/request'
 import { Redirect } from 'react-router-dom'
