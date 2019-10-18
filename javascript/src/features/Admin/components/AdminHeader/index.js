5b1e4b45e6c006f15d223b755aeb2b4340922403
Matheus Sabino <matheusouzatj@gmail.com>
2019-10-18 13:26:17 -0300

    .

--- javascript/src/features/Admin/components/AdminHeader/index.js
+++ javascript/src/features/Admin/components/AdminHeader/index.js
@@ -6,7 +6,7 @@ export default function AdminHeader() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
 
-        <a className="navbar-brand" href="#">DeliVeryFast</a>
+        <a className="navbar-brand" href="#/admin">DeliVeryFast</a>
 
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
